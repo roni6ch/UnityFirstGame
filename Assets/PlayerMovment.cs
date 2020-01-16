289a1fc4f5256a371171abbbfffca45b71942382
Roni <roni691986@gmail.com>
2020-01-16 14:32:27 +0200

    first player

---
+++ Assets/PlayerMovment.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 200f;
+    public float sidewaysForce = 200f;
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        if (Input.GetKey("d")) {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Obsticle"))
+        {
+            Debug.Log(collision.collider.name);
+        }
+    }
+}
---
+++ Assets/followPlayer.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class followPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = player.position + offset;
+    }
+
+}
