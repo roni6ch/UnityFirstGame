c018af044f9096245f2717f6488989876c38f2bf
Roni <roni691986@gmail.com>
2020-01-21 10:45:29 +0200

    game complete youtube 10

---
+++ Assets/Scripts/ExitPanel.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitPanel : MonoBehaviour
+{
+    public void Exit()
+    {
+    }
+	public Button yourButton;
+
+	void Start()
+	{
+		yourButton.GetComponent<Button>();
+		yourButton.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		Debug.Log("Quit");
+		Application.Quit();
+	}
+}
---
+++ Assets/start.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class start : MonoBehaviour
+{
+	public Button yourButton;
+
+	void Start()
+	{
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		Debug.Log("Start");
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
